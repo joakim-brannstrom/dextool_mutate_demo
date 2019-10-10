e0d0af54a5740eda958ff5a7f4f04f54ba003018
Roger A. Light <roger@atchoo.org>
2019-10-10 17:15:19 +0100

    01 broker tests with v5 support

--- test/broker/01-connect-bad-packet.py
+++ test/broker/01-connect-bad-packet.py
@@ -4,32 +4,36 @@
 
 from mosq_test_helper import *
 
-rc = 1
-mid = 2
-publish_packet = mosq_test.gen_publish("pub/qos1/test", qos=1, mid=mid, payload="message")
-puback_packet = mosq_test.gen_puback(mid)
+def do_test(proto_ver):
+    rc = 1
+    mid = 2
+    publish_packet = mosq_test.gen_publish("pub/qos1/test", qos=1, mid=mid, payload="message", proto_ver=proto_ver)
+    puback_packet = mosq_test.gen_puback(mid, proto_ver=proto_ver)
 
+    port = mosq_test.get_port()
+    broker = mosq_test.start_broker(filename=os.path.basename(__file__), port=port)
 
-port = mosq_test.get_port()
-broker = mosq_test.start_broker(filename=os.path.basename(__file__), port=port)
-
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(5)
-    sock.connect(("localhost", port))
-    sock.send(publish_packet)
-    data = sock.recv(1)
-    sock.close()
-    if len(data) == 0:
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect(("localhost", port))
+        sock.send(publish_packet)
+        data = sock.recv(1)
+        sock.close()
+        if len(data) == 0:
+            rc = 0
+    except socket.error:
         rc = 0
-except socket.error:
-    rc = 0
-finally:
-    broker.terminate()
-    broker.wait()
-    (stdo, stde) = broker.communicate()
-    if rc:
-        print(stde.decode('utf-8'))
+    finally:
+        broker.terminate()
+        broker.wait()
+        (stdo, stde) = broker.communicate()
+        if rc:
+            print(stde.decode('utf-8'))
+            print("proto_ver=%d" % (proto_ver))
+            exit(rc)
 
-exit(rc)
 
+do_test(proto_ver=4)
+do_test(proto_ver=5)
+exit(0)
