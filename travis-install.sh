493720fa4d535bc478b345705ba0fe07a20d7c5d
Roger A. Light <roger@atchoo.org>
2019-10-15 15:56:11 +0100

    Add cJSON dep to travis.

--- travis-install.sh
+++ travis-install.sh
@@ -3,12 +3,12 @@
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	sudo apt-get update -qq
-	sudo apt-get install -y debhelper libc-ares-dev libssl-dev libwrap0-dev python-all python3-all uthash-dev xsltproc docbook-xsl libcunit1-dev
+	sudo apt-get install -y debhelper libc-ares-dev libssl-dev libwrap0-dev python-all python3-all uthash-dev xsltproc docbook-xsl libcunit1-dev libcjson-dev
 fi
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	brew update
-	brew install c-ares openssl libwebsockets
+	brew install c-ares cjson openssl libwebsockets
 fi
 
 sudo pip install paho-mqtt
