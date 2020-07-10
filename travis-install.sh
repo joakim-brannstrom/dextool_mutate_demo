4379f78cec93f7a4d1de93ffb66489485df63305
Roger A. Light <roger@atchoo.org>
2020-07-10 21:30:30 +0100

    Travis: Manual install of cJSON .

--- travis-install.sh
+++ travis-install.sh
@@ -3,7 +3,9 @@
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	sudo apt-get update -qq
-	sudo apt-get install -y debhelper libc-ares-dev libssl-dev libwrap0-dev python-all python3-all uthash-dev xsltproc docbook-xsl libcunit1-dev libcjson-dev
+	sudo apt-get install -y debhelper libc-ares-dev libssl-dev libwrap0-dev python-all python3-all uthash-dev xsltproc docbook-xsl libcunit1-dev
+	git clone https://github.com/DaveGamble/cJSON
+	sudo make -C cJSON install
 fi
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
