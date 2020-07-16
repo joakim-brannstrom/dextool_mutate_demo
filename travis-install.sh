34db6e77b466617c9bb9646ff42467b4ff99ebbc
Roger A. Light <roger@atchoo.org>
2020-07-16 12:11:08 +0100

    cJSON `make install` doesn't build, so build it first.

--- travis-install.sh
+++ travis-install.sh
@@ -5,6 +5,7 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	sudo apt-get update -qq
 	sudo apt-get install -y debhelper libc-ares-dev libssl-dev libwrap0-dev python-all python3-all uthash-dev xsltproc docbook-xsl libcunit1-dev
 	git clone https://github.com/DaveGamble/cJSON
+	make -C cJSON
 	sudo make -C cJSON install
 fi
 
