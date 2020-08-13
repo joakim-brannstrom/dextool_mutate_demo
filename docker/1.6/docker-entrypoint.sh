367a282c61460a1f021df086887ad195e26fc968
Roger A. Light <roger@atchoo.org>
2020-08-13 20:08:30 +0100

    Don't quit on chmod error, it can be ignored.

--- docker/1.6/docker-entrypoint.sh
+++ docker/1.6/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Set permissions
 user="$(id -u)"
 if [ "$user" = '0' ]; then
-	[ -d "/mosquitto" ] && chown -R mosquitto:mosquitto /mosquitto
+	[ -d "/mosquitto" ] && chown -R mosquitto:mosquitto /mosquitto || true
 fi
 
 exec "$@"
--- docker/local/docker-entrypoint.sh
+++ docker/local/docker-entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/ash
 set -e
 
-docker_set_permissions() {
-	local user; user="$(id -u)"
-
-	if [ "$user" = '0' ]; then
-		chown -R mosquitto:mosquitto /mosquitto
-	fi
-}
-
-
-docker_set_permissions()
+# Set permissions
+user="$(id -u)"
+if [ "$user" = '0' ]; then
+	[ -d "/mosquitto" ] && chown -R mosquitto:mosquitto /mosquitto || true
+fi
 
 exec "$@"
