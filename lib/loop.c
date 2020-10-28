add355a78b1035028dda190341b1726a6b4114ba
raspopov <raspopov@cherubicsoft.com>
2020-10-28 18:30:54 +0300

    QNX has no pselect() either.
    Signed-off-by: raspopov <raspopov@cherubicsoft.com>

--- lib/loop.c
+++ lib/loop.c
@@ -30,7 +30,7 @@ Contributors:
 #include "tls_mosq.h"
 #include "util_mosq.h"
 
-#if !defined(WIN32) && !defined(__SYMBIAN32__)
+#if !defined(WIN32) && !defined(__SYMBIAN32__) && !defined(__QNX__)
 #define HAVE_PSELECT
 #endif
 
