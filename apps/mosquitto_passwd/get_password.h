a4389fc9ce9a21442b36a14305648be79febd91e
Roger A. Light <roger@atchoo.org>
2021-01-20 11:46:18 +0000

    Fix SPDX identifiers: EDL-1.0 -> BSD-3-Clause.
    The two licenses are the same.

--- apps/mosquitto_passwd/get_password.h
+++ apps/mosquitto_passwd/get_password.h
@@ -12,7 +12,7 @@ The Eclipse Public License is available at
 and the Eclipse Distribution License is available at
   http://www.eclipse.org/org/documents/edl-v10.php.
  
-SPDX-License-Identifier: EPL-2.0 OR EDL-1.0
+SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
 
 Contributors:
    Roger Light - initial implementation and documentation.
