dd4a64b049f21cacc6fdb8988f8e515a136a31cd
Abilio Marques <abiliojr@gmail.com>
2021-03-22 08:15:47 +0100

    fully empty socketpairR on interruptible_sleep
    Signed-off-by: Abilio Marques <abiliojr@gmail.com>

--- lib/loop.c
+++ lib/loop.c
@@ -204,10 +204,9 @@ static int interruptible_sleep(struct mosquitto *mosq, time_t reconnect_delay)
 	int maxfd = 0;
 
 #ifndef WIN32
-	if(read(mosq->sockpairR, &pairbuf, 1) == 0){
-	}
+	while(read(mosq->sockpairR, &pairbuf, 1) > 0);
 #else
-	recv(mosq->sockpairR, &pairbuf, 1, 0);
+	while(recv(mosq->sockpairR, &pairbuf, 1, 0) > 0);
 #endif
 
 	local_timeout.tv_sec = reconnect_delay;
